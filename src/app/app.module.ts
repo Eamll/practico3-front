8314cbdc24e9ec287d242d97788bf824dc064658
Eamll <alexis.0804yes@gmail.com>
2024-09-29 09:31:21 -0400

    [FEAT] Add PrimeNG library and create components for client and sales management features. Implement home route and basic styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListaClientesComponent } from './components/lista-clientes/lista-clientes.component';
+import { RegistroClienteComponent } from './components/registro-cliente/registro-cliente.component';
+import { ListaVentasComponent } from './components/lista-ventas/lista-ventas.component';
+import { RegistroVentaComponent } from './components/registro-venta/registro-venta.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaClientesComponent,
+    RegistroClienteComponent,
+    ListaVentasComponent,
+    RegistroVentaComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/lista-ventas/lista-ventas.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-ventas',
+  templateUrl: './lista-ventas.component.html',
+  styleUrl: './lista-ventas.component.css'
+})
+export class ListaVentasComponent {
+
+}
